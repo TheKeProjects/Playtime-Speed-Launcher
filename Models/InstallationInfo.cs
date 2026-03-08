a2ad89cb49ed90c7bcd7c7109ae75c85f99853db
KeProjects <88108619+TheKeProjects@users.noreply.github.com>
2026-03-08 23:24:19 +0100

    Add files via upload

---
+++ Models/InstallationInfo.cs
@@ -0,0 +1,8 @@
+namespace SpeedrunLauncher.Models;
+
+public class InstallationInfo
+{
+    public string Name { get; set; } = "";
+    public string ExePath { get; set; } = "";
+    public string? IconPath { get; set; }
+}
