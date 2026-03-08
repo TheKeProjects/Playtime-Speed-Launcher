ceb3382dc791b099e5cff277d0a23263c13b2ad6
KeProjects <88108619+TheKeProjects@users.noreply.github.com>
2026-03-08 23:21:50 +0100

    Delete Models directory

--- Models/InstallationInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace SpeedrunLauncher.Models;
-
-public class InstallationInfo
-{
-    public string Name { get; set; } = "";
-    public string ExePath { get; set; } = "";
-    public string? IconPath { get; set; }
-}
