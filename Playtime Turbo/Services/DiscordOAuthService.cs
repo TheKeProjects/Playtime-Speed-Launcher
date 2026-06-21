5b945d7540cab2e4635788145553c30606b8cb21
An Average Developer <thekeprojectsoficial@gmail.com>
2026-06-21 20:39:15 +0200

    Update

--- Playtime Turbo/Services/DiscordOAuthService.cs
+++ Playtime Turbo/Services/DiscordOAuthService.cs
@@ -21,7 +21,7 @@ namespace SpeedrunLauncher.Services;
 
 public static class DiscordOAuthService
 {
-    private const string ClientId    = "956862634945310750";
+    private const string ClientId    = "YourClientId";
     private const int    Port        = 45678;
     public  const string RedirectUri = "http://localhost:45678/callback";
 
